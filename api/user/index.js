9cf060e59e2342e6a367f9f35c535876f2334655
Calvin Cai <yuncai@microsoft.com>
2023-08-16 08:14:36 +0800

    update js

--- api/user/index.js
+++ api/user/index.js
@@ -2,12 +2,10 @@ module.exports = async function (context, req) {
   const header = req.headers['x-ms-client-principal'];
   const encoded = Buffer.from(header, 'base64');
   const decoded = encoded.toString('ascii');
-
-  //const user = req.body || {};
-
+  
   context.res = {
     body: {
-      clientPrincipal: JSON.parse(decoded) //,accessToken: user.accessToken
+      clientPrincipal: JSON.parse(decoded)
     },
   };
 };
