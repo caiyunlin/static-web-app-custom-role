b8b8d12ffdb1ee837551718b8b55134bc84e7e84
Calvin Cai <cylin2000@163.com>
2024-11-01 16:29:03 +0800

    Update index.js

--- api/GetRoles/index.js
+++ api/GetRoles/index.js
@@ -18,7 +18,7 @@ module.exports = async function (context, req) {
    
 
     roles.push("test"); // output test role for testing
-    roles.push(user.accessToken); // output accessToken to role for testing
+    //roles.push(user.accessToken); // output accessToken to role for testing
     roles.push("test2");
 
     for (const [role, groupId] of Object.entries(roleAppRoleMapping)) {
